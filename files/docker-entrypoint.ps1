5c8b8a8e8d12f5416b39ba485c65e66d333bc91b
Raul <rauljmz@gmail.com>
2018-09-07 15:50:09 +0800

    Fix remote debugging not started in sitecore service

--- files/docker-entrypoint.ps1
+++ files/docker-entrypoint.ps1
@@ -1,3 +1,2 @@
-C:\Scripts\Watch-Directory.ps1 -Path 'C:\workspace' -Destination 'C:\inetpub\wwwroot\sitecore'
-C:\Scripts\Start-RemoteDebug.ps1
-C:\ServiceMonitor.exe w3svc
+& C:\Scripts\Start-RemoteDebug.ps1
+& C:\Scripts\Watch-Directory.ps1 -Path 'C:\workspace' -Destination 'C:\inetpub\wwwroot\sitecore'
